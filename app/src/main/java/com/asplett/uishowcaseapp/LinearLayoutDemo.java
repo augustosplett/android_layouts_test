66c015efce1df5dd4f3acab88f5c6a8f66fd7e93
Augusto Madeira Splett (Tinho) <augusto.splett@gmail.com>
2024-09-29 11:47:43 -0400

    created the label, input text and button on page1

--- app/src/main/java/com/asplett/uishowcaseapp/LinearLayoutDemo.java
+++ app/src/main/java/com/asplett/uishowcaseapp/LinearLayoutDemo.java
@@ -4,13 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 public class LinearLayoutDemo extends Fragment {
 
+    private LinearLayout page1;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.linear_layout_demo, container, false);
+
+        View view = inflater.inflate(R.layout.linear_layout_demo, container, false);
+
+        page1 = view.findViewById(R.id.page1);
+        Button page1_button = view.findViewById(R.id.page1_button);
+
+        page1_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(), "Button Clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
