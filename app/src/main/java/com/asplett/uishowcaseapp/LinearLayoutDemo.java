3b9101eee8aef291ad6074fd9a3bfd2e8cb80d93
Augusto Madeira Splett (Tinho) <augusto.splett@gmail.com>
2024-09-29 14:27:07 -0400

    finished page 1

--- app/src/main/java/com/asplett/uishowcaseapp/LinearLayoutDemo.java
+++ app/src/main/java/com/asplett/uishowcaseapp/LinearLayoutDemo.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
 public class LinearLayoutDemo extends Fragment {
 
     private LinearLayout page1;
+    private EditText userInput;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -20,13 +22,14 @@ public class LinearLayoutDemo extends Fragment {
         View view = inflater.inflate(R.layout.linear_layout_demo, container, false);
 
         page1 = view.findViewById(R.id.page1);
+        userInput = view.findViewById(R.id.page1_edit_text);
         Button page1_button = view.findViewById(R.id.page1_button);
 
         page1_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(getActivity(), "Button Clicked!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "hello, " + userInput.getText(), Toast.LENGTH_SHORT).show();
             }
         });
 
